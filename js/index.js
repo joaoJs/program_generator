4d0938274d252cb7ae8e5adbac1c4a15ec7ef0d8
Joao Campos <j.campos4g@gmail.com>
2017-07-25 09:34:14 -0400

    added comments

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
+// Create global variables to store composers and pieces for program
 var composers = [];
 var pieces = []; 
 
+// Data from where composers and pieces are randomly chosen
 var data = [ 
 	{
 	"composer": {
@@ -9,7 +11,7 @@ var data = [
 		"bio": "https://en.wikipedia.org/wiki/Alexander_Scriabin"
 	},
 	"compositions": {
-		"work": ["Sonata no 2 in g-sharp minor op.19", "Sonata no 4 in F-sharp major op.19", "Sonata no 5 op.53", "Sonata No. 7 'White Mass', Op. 64" , "Sonata No. 9 'Black Mass' op.68", "Sonata no 10 op. 70"]
+		"work": ["Fantasie op.28","Sonata no 2 in g-sharp minor op.19", "Sonata no 4 in F-sharp major op.19", "Sonata no 5 op.53", "Sonata No. 7 'White Mass', Op. 64" , "Sonata No. 9 'Black Mass' op.68", "Sonata no 10 op. 70"]
 	}
 },
 	{
@@ -19,7 +21,7 @@ var data = [
 		"bio": "https://en.wikipedia.org/wiki/Johann_Sebastian_Bach"
 	},
 	"compositions": {
-		"work": ["Partita No. 1 in B♭ major, BWV 825", "Partita No. 2 in C minor, BWV 826", "Partita No. 3 in A minor, BWV 827", "Partita No. 4 in D major, BWV 828", "Partita No. 5 in G major, BWV 829", "Partita No. 6 in E minor, BWV 830"] 
+		"work": ["Aria Variata alla Maniera Italiana","Partita No. 1 in B♭ major, BWV 825", "Partita No. 2 in C minor, BWV 826", "Partita No. 3 in A minor, BWV 827", "Partita No. 4 in D major, BWV 828", "Partita No. 5 in G major, BWV 829", "Partita No. 6 in E minor, BWV 830"] 
 	}
 },
 	{
@@ -39,7 +41,7 @@ var data = [
 		"bio": "https://en.wikipedia.org/wiki/Franz_Schubert"
 	},
 	"compositions": {
-		"work": ["Sonata in A minor, D. 784", "Sonata in G major, D. 894", "Sonata in C minor, D. 958", "Sonata in A major, D. 959", "Sonata in B-flat major, D. 960"] 
+		"work": ["4 Impromptus op.142", "Sonata in A minor, D. 784", "Sonata in G major, D. 894", "Sonata in C minor, D. 958", "Sonata in A major, D. 959", "Sonata in B-flat major, D. 960"] 
 	}
 },	
 	{
@@ -49,7 +51,7 @@ var data = [
 		"bio": "https://en.wikipedia.org/wiki/Johannes_Brahms"
 	},
 	"compositions": {
-		"work": ["Op. 116, 7 fantasies", "Op. 117, 3 intermezzos", "Op. 118, 6 piano pieces", "Op. 119, 4 piano pieces"]
+		"work": ["Sonata No.3 in f minor, op.5", "Op. 116, 7 fantasies", "Op. 117, 3 intermezzos", "Op. 118, 6 piano pieces", "Op. 119, 4 piano pieces"]
 	}
 },	
 	{
@@ -109,7 +111,7 @@ var data = [
 		"bio": "https://en.wikipedia.org/wiki/Robert_Schumann"
 	},
 	"compositions": {
-		"work": ["Novelletten op.21", "Faschingsschwank aus Wien Op.26"]
+		"work": ["Fantasiestücke, Op.12","Fantasie op.17","Symphonic Etudes op.13","Novelletten op.21", "Faschingsschwank aus Wien Op.26"]
 	}
 },	
 	{
@@ -136,7 +138,7 @@ var data = [
 	"composer": {
 		"name":"George Walker",
 		"nationality": "American",
-		"bio": "https://en.wikipedia.org/wiki/George_Walker_(composer)"
+		"bio": "http://georgetwalker.com/"
 	},
 	"compositions": {
 		"work": ["Piano Sonata No. 1", "Piano Sonata No. 2", "Piano Sonata No. 3", "Piano Sonata No. 4", "Piano Sonata No. 5"]
@@ -146,7 +148,7 @@ var data = [
 	"composer": {
 		"name":"Leoš Janáček",
 		"nationality": "Czech",
-		"bio": "https://en.wikipedia.org/wiki/Leo%C5%A1_Jan%C3%A1%C4%8Dek"
+		"bio": "http://www.leosjanacek.co.uk/biography/"
 	},
 	"compositions": {
 		"work": ["Piano Sonata 1"]
@@ -169,7 +171,7 @@ var data = [
 		"bio": "https://en.wikipedia.org/wiki/Gy%C3%B6rgy_Ligeti"
 	},
 	"compositions": {
-		"work": ["Musica Ricercata", "Selection from Etudes"]
+		"work": ["Musica Ricercata", "Etudes"]
 	}
 },	
 	{
@@ -181,46 +183,156 @@ var data = [
 	"compositions": {
 		"work": ["Sequenza IV"]
 	}
-},			
-]
+},	
+	{
+	"composer": {
+		"name":"George Crumb",
+		"nationality": "American",
+		"bio": "https://en.wikipedia.org/wiki/George_Crumb#Piano"
+	},
+	"compositions": {
+		"work": ["Makrokosmos, Volume I","Makrokosmos, Volume II"]
+	}
+},	
+	{
+	"composer": {
+		"name":"Aleksandër Peçi ",
+		"nationality": "Albanian",
+		"bio": "https://en.wikipedia.org/wiki/Aleksand%C3%ABr_Pe%C3%A7i"
+	},
+	"compositions": {
+		"work": ["Cartesius Cantus"]
+	}
+},	
+	{
+	"composer": {
+		"name":"Rachmaninoff",
+		"nationality": "Russian",
+		"bio": "https://en.wikipedia.org/wiki/Sergei_Rachmaninoff"
+	},
+	"compositions": {
+		"work": ["Piano Sonata No. 1 in D minor, Op. 28", "Études-Tableaux, Op. 33", "Études-Tableaux, Op. 39"]
+	}
+},	
+	{
+	"composer": {
+		"name":"Claude Debussy",
+		"nationality": "French",
+		"bio": "https://en.wikipedia.org/wiki/Claude_Debussy"
+	},
+	"compositions": {
+		"work": ["Preludes Book 1", "Preludes Book 2","Berceuse Heroique"]
+	}
+},	
+	{
+	"composer": {
+		"name":"Maurice Ravel",
+		"nationality": "French",
+		"bio": "https://en.wikipedia.org/wiki/Maurice_Ravel"
+	},
+	"compositions": {
+		"work": ["Gaspard de la nuit"]
+	}
+},
+	{
+	"composer": {
+		"name":"Franz Liszt",
+		"nationality": "Hungarian",
+		"bio": "https://en.wikipedia.org/wiki/Franz_Liszt"
+	},
+	"compositions": {
+		"work": ["Selection from Transcendental Etudes","Nuages Gris","Années de pèlerinage. Première année: Suisse"]
+	}
+},	
+	{
+	"composer": {
+		"name":"Frédéric Chopin",
+		"nationality": "Polish",
+		"bio": "https://en.wikipedia.org/wiki/Piano_sonatas_(Chopin)"
+	},
+	"compositions": {
+		"work": ["Piano Sonata No. 2 in B-flat minor, Op. 35","Piano Sonata No. 3 in B minor, Op. 58"]
+	}
+},	
+	{
+	"composer": {
+		"name":"Rodrigo Bussad",
+		"nationality": "Brazilian",
+		"bio": "https://www.rodrigobussad.com/"
+	},
+	"compositions": {
+		"work": ["Moon Inside I - Piano Cycle"]
+	}
+},		
+	{
+	"composer": {
+		"name":"Ermir Bejo",
+		"nationality": "Albanian",
+		"bio": "https://www.google.com/#q=ermir+bejo"
+	},
+	"compositions": {
+		"work": ["4 Pieces op.1"]
+	}
+},	
+];
 
+// when DOM is loaded 
 $(document).ready(function() {
   
+  // when form is submitted
   $("#click").on("click", function() {
    
+   // if no programs are currently displayed on the page
     if ($('#result').is(':empty')){
       
+    // store the input from the user in variable quant
     var quant = $("#quantity").val();
     
-    if (!quant) {
-      $("#error").html("Please specify quantity");
+    // ensure user provide input and input is greater than 0
+    if (!quant || quant == 0) {
+      $("#error").html("Must learn at least one piece");
+    // ensure input contains only numeric values
     } else if (/[^0-9]/g.test(quant)) {
-      $("#error").html("Must input only numeric values")
+      $("#error").html("Must input only numeric values");
+    // ensure input in not greater than 15
+    } else if (quant > 15) {
+      $("#error").html("Can't learn that many pieces!"); 
+    // if all criterias are met, clear any error messages and proceed         
     } else {
     
     $("#error").empty();   
       
+    // get n composers when n equals number of pieces to learn
     for (let i = 0; i < quant; i++) {
-      
+      // use helper function to push composers to composers array
       getComposers(data);
       
     }  
-      
+    
+    // initialize an unordered list to append to HTML  
     var ul = "<ul id='ul'>";
-      
+    
+    // for each composer  
     composers.forEach((c,i) => {
+   	// make a new list item  with composer's name and pieces. in the ul
+   	// also, add url to composer's info
       ul += "<li><a href="+c[1]+" target='_blank'>"+c[0]+"</a><p id='piece'>"+pieces[i]+"</p></li>"
     });
-      
-    ul += "</ul><button id='clear' class='btn btn-primary'>Clear</button>";
     
+    // close ul and add a clear button to clear current program  
+    ul += "</ul><button id='clear' class='btn btn-danger'>Clear</button>";
+    
+    // append ul to div result in the html document
     $("#result").html(ul);
-      
+    
+    // when user clicks on clear 
     $("#clear").on("click", function() {
-      
+      	
+      	// empty composer and pieces arrays
         composers = [];
         pieces = [];
         
+        // empty any programs, error messages or quantity from the page
         $("#result").empty();
         $("#error").empty();
         $("#quantity").val("");
@@ -228,7 +340,7 @@ $(document).ready(function() {
       });
     
     }
-      
+    // ensure user clears current program 
     } else {
       $("#error").html("Please clear current program")
     }
@@ -237,6 +349,7 @@ $(document).ready(function() {
   
 });
 
+// helper function that gets a random composer
 function getComposers(data) {
   
   var rand = Math.floor(Math.random() * data.length);
@@ -244,24 +357,28 @@ function getComposers(data) {
     
     composers.push([composer.name, composer.bio]);
   
-    getPiece(data[rand]);
-  
-  
-  
+    getPiece(data[rand]);  
 }
 
+
+// helper function that gets a random piece
 function getPiece(composer) {
    
     var piece =
 composer.compositions.work;
     var rand2 = Math.floor(Math.random() * piece.length);
     var final = piece[rand2];
-  
+  	
+  	// ensure piece is not already included in program
     if (!pieces.includes(final)) {
       pieces.push(final);
-    } else if (pieces.includes(final) && piece.length === 1) {
+    // if piece is already in program and composer has less than 4 pieces in data, 
+    // choose a new composer 
+    } else if (pieces.includes(final) && piece.length < 4) {
       composers.pop();
       getComposers(data);
+    // if piece is already in program but composer has more than one piece in data
+    // get another piece recursively until piece is not in program
     } else {
       getPiece(composer);
     }
